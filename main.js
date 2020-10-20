0185684b1d5413207a2fa9e460db1d5ff0b2eb2e
Timothy Do <tydo83@gmail.com>
2020-10-20 13:07:36 -0400

    ternary done

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 // Your code below
+const isItColin = function(name) {
+  return name === 'Colin' ? 'Yes, that is Colin.' : 'No, that is not Colin.';
+}
 
+const isHot = function(temperature) {
+  return temperature >= 80 ? 'Yes, it is indeed hot.' : 'No, it is not hot.';
+}
+
+const helloThere = function(name) {
+  return name.length > 5 ? `Hi, ${name}` : `Hello, ${name}`;
+}
+
+const goodbyeYou = function(name) {
+  return name !== undefined ? `Goodbye, ${name}` : 'Goodbye, stranger';
+}
+
+const darkAndStormyNight = function(light, rainfall) {
+  return light === 'dark' && rainfall > 0.5 ? 'It was a dark and stormy night.' : "My usual opening doesn't apply, I guess."
+}
 
 
 
